f7afca10963ba6df873dbf3248ee6448ca5ace4d
Eldar Allakhyarov <tripenyazaraz@gmail.com>
2023-09-15 11:47:37 +0600

    Initial commit

---
+++ src/ui/mainForm.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javax.swing.*;
+
+public class mainForm extends JFrame {
+    private JPanel rootPanel;
+    private JButton hewwoButton;
+
+    public mainForm() {
+        setContentPane(rootPanel);
+        setVisible(true);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new mainForm();
+    }
+}
